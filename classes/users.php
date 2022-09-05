c113705144dc78a81c25c714c2c68302a0bea44d
Franpan94 <francescopanico94@libero.it>
2022-09-05 15:45:34 +0200

    Print on screen

---
+++ classes/users.php
@@ -0,0 +1,41 @@
+<?php
+
+class users {
+
+   protected $name;
+   protected $type;
+
+   public function __construct($_name, $_type){
+    
+        $this -> name = $_name;
+        $this -> type = $_type;
+
+   }
+
+   public function getName(){
+
+      return $this -> name;
+
+   }
+
+   public function setName($_name){
+
+      $this -> name = $_name;
+
+   }
+
+   public function getType(){
+
+      return $this -> type;
+
+   }
+
+   public function setType($_type){
+
+      $this -> type = $_type;
+
+   }
+
+
+}
+
